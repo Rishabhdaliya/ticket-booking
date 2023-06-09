cdbd66f48f40dd0da70b1e02c80f8f4fd6eaf2ad
rishabh.daliya <rishabh.daliya@schbang.com>
2023-06-09 16:45:43 +0530

    fix: table alignment

--- src/Components/Dashboard/Reservations.jsx
+++ src/Components/Dashboard/Reservations.jsx
@@ -37,19 +37,19 @@ const Reservations = () => {
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 text-center py-3">
                   Name
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 text-center py-3">
                   Email
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 text-center py-3">
                   Seat No
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 text-center py-3">
                   Date Of Booking
                 </th>
-                <th scope="col" className="px-6 py-3">
+                <th scope="col" className="px-6 text-center py-3">
                   Actions
                 </th>
               </tr>
